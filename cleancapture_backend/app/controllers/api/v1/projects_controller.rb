33d5acd02d430f9d02434e770a6a12694f2d9953
Giulio de Cadilhac <g.decadilhac@gmail.com>
2023-05-16 20:13:09 +0100

    reset due to db error and started porjects controller

--- cleancapture_backend/app/controllers/api/v1/projects_controller.rb
+++ cleancapture_backend/app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,28 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :set_projects, only: [:index]
+
   def index
-    projects = Project.all
-    render json: projects
+    # render json: projects
+    render json: { projects: @projects }, status: :ok
+  end
+
+  def show
+    project = Project.find(params[:id])
+    render json: { project: project }, status: :ok
+  end
+
+  def create
+    project = Project.new(project_params)
+    if project.save
+      render json: { project: project }, status: :created
+    else
+      render json: { errors: project.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_projects
+    @projects = Project.all
   end
 end
